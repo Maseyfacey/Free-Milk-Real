e67a342ff5942f98b683f27827d3ebfb4bce7f42
Maseyfacey <153440666+Maseyfacey@users.noreply.github.com>
2025-10-30 11:39:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const milk = document.getElementById('milk');
+const sound = document.getElementById('milkSound');
+
+nugget.addEventListener('click', () => {
+  sound.currentTime = 0; // restart if already playing
+  sound.play();
+});
